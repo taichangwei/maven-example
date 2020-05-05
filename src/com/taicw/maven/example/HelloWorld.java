1ade6f615d968a7c1c2d512d1061c4238b6c75f6
taicw <taicw@163.com>
2020-05-05 09:59:33 +0800

    创建项目

---
+++ src/com/taicw/maven/example/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.taicw.maven.example;
+
+
+/**
+ * @author taicw
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
